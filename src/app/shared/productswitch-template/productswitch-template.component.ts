be6c5f710da2ca1cb4d6600a177bf382454ab3a7
shekela <lukashekelashvili@gmail.com>
2024-12-12 11:25:07 +0100

    respo-part-second

--- src/app/shared/productswitch-template/productswitch-template.component.ts
+++ src/app/shared/productswitch-template/productswitch-template.component.ts
@@ -13,10 +13,6 @@ export class ProductswitchTemplateComponent {
   products: BackgroundContentModel[] = ProductsToChoose; // Products array
   currentProduct: BackgroundContentModel = this.products[0];
 
-  widthFull = (this.products.length * 178.333).toString() + "px";
-  widthSection = ((100 / this.products.length) - 1).toString() + "%"
-  
-
   selectProduct(product: BackgroundContentModel): void {
     this.currentProduct = product; // Update the current product
     console.log('Selected product:', this.currentProduct);
