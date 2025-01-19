491258cbb4f03bddeb24ddc2a9d62f1dc59b5239
shekela <lukashekelashvili@gmail.com>
2025-01-19 16:24:42 +0100

    Branding v1.2

--- src/app/DUMMY_DATA/company-info.ts
+++ src/app/DUMMY_DATA/company-info.ts
@@ -1,19 +1,19 @@
 import { CompanyModel } from "../models/brand.model";
 
 export const CompanyData: CompanyModel = {
-   logo: "assets/signal-2025-01-19-182351-removebg-preview.png",
+   logo: "assets/pani-logo.png",
    mondayTime: "9:00AM - 9:00PM",
    tuesdayTime: "9:00AM - 9:00PM",
    wednesdayTime: "9:00AM - 9:00PM",
    thursdayTime: "9:00AM - 9:00PM",
    fridayTime: "9:00AM - 9:00PM",
    saturdayTime: "9:00AM - 9:00PM",
-   sundayTime: "NOT WORKING",
+   sundayTime: "9:00AM - 9:00PM",
    email: "pm@paniacoustics.com",
-   mobile: "+995 557 778 778",
-   instagram: "https://www.instagram.com/pani_acoustics",
-   instagramPageName: "pani_acoustics",
-   facebook: "https://www.facebooks.com/pani_acoustics",
+   mobile: "+995 585 890 407",
+   instagram: "https://www.instagram.com/pani.acoustics?igsh=MXJtcXdidHJyYTFpNA==",
+   instagramPageName: "pani.acoustics",
+   facebook: "https://www.facebook.com/acousticpanell?mibextid=wwXIfr&rdid=VpafI7VzbRfhQDyb&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1Xyg86AYyM%2F%3Fmibextid%3DwwXIfr",
    aboutUsEn: "Pani Acoustics is a leading provider of premium acoustic panels based in Tbilisi, Georgia. We specialize in creating innovative solutions for sound control and noise reduction, offering products designed to enhance spaces with style and functionality. Whether for homes, offices, or studios, Pani Acoustics delivers exceptional quality and performance.",
    aboutUsKa: "Pani Acoustics არის აკუსტიკური პანელების წამყვანი მიმწოდებელი, რომელიც მდებარეობს თბილისში, საქართველოში. ჩვენ ვთავაზობთ ინოვაციურ გადაწყვეტილებებს ხმაურის კონტროლისთვის და ხმის იზოლაციისთვის. ჩვენი პროდუქცია შექმნილია სივრცის ესთეტიკისა და ფუნქციონალის გასაუმჯობესებლად. Pani Acoustics გთავაზობთ უმაღლესი ხარისხის და შესრულების გარანტიას.",
    aboutUsRu: "Pani Acoustics — ведущий поставщик акустических панелей, расположенный в Тбилиси, Грузия. Мы предлагаем инновационные решения для контроля звука и снижения шума, создавая продукцию, которая сочетает стиль и функциональность. Для дома, офиса или студии Pani Acoustics обеспечивает высочайшее качество и надежность."
